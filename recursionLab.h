2048066f93338064c8919d859bd13d0829e48478
dnatedauphinais <dnatedauphinais@gmail.com>
2024-07-01 22:12:53 +0000

    updated code

--- recursionLab.h
+++ recursionLab.h
@@ -8,14 +8,25 @@ using namespace std;
 //          lineOfStars(0) returns ""
 // You may not use a loop.
 string lineOfStars(int n) {
-	return "fixme";
+	int num =n;
+	if(num==0){
+		return "";
+	}
+	else{
+		string s = *;
+		return "s"+lineOfStars(num--);
+	}
 }
 
 // Write a function power() that will
 //    compute the x to the power of n recursively
 // if the power is invalid, return 0
 float power(float x, int n) {
-	return -1;	
+	if(n<=0)
+	return 0;
+	else{
+		return x*(power(n--));
+	}
 }
 
 /*
@@ -35,7 +46,13 @@ should display:
 ****
 
 */
-void stars(int n) {}
+void stars(int n) {
+if(n<=0)
+return;
+else{
+		return lineOfStars(4)+lineOfStars(3)+lineOfStars(2)+lineOfStars(1)+lineOfStars+lineOfStars(2)+lineOfStars(3)+lineOfStars(4);
+}
+}
 
 // Write a *recursive* function that will check if
 //    a string is a palindrome
@@ -43,7 +60,20 @@ void stars(int n) {}
 // Hint 2: s.substr(i,j) gives you the substring of s. For example if s = Hello, s(1,s.length()-1) gives you ello
 bool isPalindromeR(string s)
 {
+	bool b =false;
+	if(s=="")
 	return false;
+	else{
+		if(s.front()==s.back()){
+		b = true;
+		s = s(1,s.length()-1);
+		s = s(s.back,s.length()-1);
+		isPalindromeR(s);
+		}
+		else
+		b=false;
+	return b;
+	}
 }
 
 
@@ -52,5 +82,12 @@ bool isPalindromeR(string s)
 //  methods from the previous function
 string reverse(string s)
 {
-	return "fixme"; 
+	int num =0
+	if(s=="")
+	return "";
+	else{
+		p.substr(num,1)=reverse(s.substr(s.length()--,1));
+		return p;
+		}
+	}
 }
